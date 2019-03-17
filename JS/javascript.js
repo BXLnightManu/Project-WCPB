2caa7e0b96f3bf5d7ee2b29d86f3a2b70a03f728
Gaston Ndanyuzwe <ndanyuzwe@hotmail.com>
2019-03-17 17:58:46 +0100

    refactor js code to make it functional

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,56 +1,50 @@
-/*(() => {
-    document.addEventListener('DOMContentLoaded', () => {
-        var poster = document.getElementsByClassName("poster");
-        console.log(poster)
-for(var x=0; x < poster.length; x++) {
-    poster[x].onmouseover= function() {mouseOver()};
-    function mouseOver() {
-        poster[x].style.opacity = "1";
-    }
-    poster[x].onmouseout = function() {mouseOut()};
-    function mouseOut() {
-        poster[x].style.opacity = "0.5";
-    }
-}
-var wzdtatus = document.getElementsByClassName("wzdtatus");
-for(var x=0; x < wzdtatus.length; x++) {
-    wzdtatus[x].onmouseover= function() {mouseOver()};
-    function mouseOver() {
-        wzdtatus[x].style.backgroundColor = "transparent";
-        wzdtatus[x].style.color = "transparent";
-    }
-    wzdtatus[x].onmouseout = function() {mouseOut()};
-    function mouseOut() {
-        wzdtatus[x].style.backgroundColor = "rgba(224,224,224,0.8)";
-        wzdtatus[x].style.color = "rgba(0,0,0)";
-    }
-}
+(() => {
+    document.addEventListener("DOMContentLoaded", () => {
+        const posters = document.getElementsByClassName("poster");
+        for (const poster of posters) {
+            poster.onmouseover = function() {
+                poster.style.opacity = "1";
+            };
 
-var wzdname = document.getElementsByClassName("wzdname");
-for(var x=0; x < wzdname.length; x++) {
-    wzdname[x].onmouseover = function() {mouseOver()};
-    function mouseOver() {
-        wzdname[x].style.fontsize = "15px";
-        wzdname[x].style.top = "80px";
-    }
-    wzdname[x].onmouseout = function() {mouseOut()};
-    function mouseOut() {
-        wzdname[x].style.fontsize = "25px";
-        wzdname[x].style.top = "0px";
-    }
-}
+            poster.onmouseout = function() {
+                poster.style.opacity = "0.5";
+            };
+        }
 
-var wzddesc = document.getElementsByClassName("wzddesc");
-for(var x=0; x < wzddesc.length; x++) {
-    wzddesc[x].onmouseover = function() {mouseOver()};
-    function mouseOver() {
-        wzddesc[x].style.color = "transparent";
-    }
-    wzddesc[x].onmouseout = function() {mouseOut()};
-    function mouseOut() {
-        wzddesc[x].style.color = "rgb(255,255,255)";
-    }
-}
-    })
+        const wzdtatuses = document.getElementsByClassName("wzdtatus");
+        for (const wzdtatus of wzdtatuses) {
+            wzdtatus.onmouseover = function() {
+                wzdtatus.style.backgroundColor = "transparent";
+                wzdtatus.style.color = "transparent";
+            };
+            wzdtatus.onmouseout = function() {
+                wzdtatus.style.backgroundColor = "rgba(224,224,224,0.8)";
+                wzdtatus.style.color = "rgba(0,0,0)";
+            };
+        }
+
+        const wzdnames = document.getElementsByClassName("wzdname");
+        for (const wzdname of wzdnames) {
+            wzdname.onmouseover = function() {
+                wzdname.style.fontsize = "15px";
+                wzdname.style.top = "80px";
+            };
+
+            wzdname.onmouseout = function() {
+                wzdname.style.fontsize = "25px";
+                wzdname.style.top = "0px";
+            };
+        }
+
+        const wzddescs = document.getElementsByClassName("wzddesc");
+        for (const wzddesc of wzddescs) {
+            wzddesc.onmouseover = function() {
+                wzddesc.style.color = "transparent";
+            };
+
+            wzddesc.onmouseout = function() {
+                wzddesc.style.color = "rgb(255,255,255)";
+            };
+        }
+    });
 })();
-*/
